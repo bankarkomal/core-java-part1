61f8716ec0cf1b0ac49b40f74d1de6525e3fc410
komal <bankarkomal38@gmail.com>
2023-06-16 13:59:48 +0530

    Control-Steatment SwitchExample.java

---
+++ conditional-steatment/control-steatment/src/controlsteatment/SwitchExample.java
@@ -0,0 +1,24 @@
+package controlsteatment;
+
+import java.util.Scanner;
+
+/**
+ * This class demonstreate the use of control steatment
+ * In this class we learn the swtich Examples
+ * Now I Want to class name here{ @code switch example}
+ */
+/*
+  This is multi line comment
+  A new line comment for testing
+ */
+public class SwitchExample {
+    public static void main(String[] args) {
+        System.out.println(" Plese enter the string:");
+        //user input
+        Scanner Scanner = new Scanner(System.in);
+        String input = Scanner.nextLine();
+        System.out.println("enter the string:"+ input);
+
+    }
+
+}
