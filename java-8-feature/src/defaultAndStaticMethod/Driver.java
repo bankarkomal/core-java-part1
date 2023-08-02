c7c8eb3223e6aa75f023f5e471a0561584952f20
komal <bankarkomal38@gmail.com>
2023-08-02 14:04:17 +0530

    Java-8-feature DefaultAndStaticMethod

---
+++ java-8-feature/src/defaultAndStaticMethod/Driver.java
@@ -0,0 +1,37 @@
+package defaultAndStaticMethod;
+class Test implements ExampleInterface{
+    @Override
+    public int randomCalculate(int a, int b) {
+        return a+b;
+    }
+
+//    @Override
+//    public void print() {
+//
+//    }
+
+    @Override
+    public void print() {
+        ExampleInterface.super.print();
+    }
+}
+class Test2 implements ExampleInterface {
+
+    @Override
+    public int randomCalculate(int a, int b) {
+        return a*b;
+    }
+
+//    @Override
+//    public void print() {
+//
+//    }
+}
+
+
+public class Driver {
+    public static void main(String[] args) {
+        ExampleInterface.showMsg();
+    }
+}
+
