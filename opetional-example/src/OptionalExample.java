2acbd45f6b48e7f2f58edbb3fac51f44a65b402d
komal <bankarkomal38@gmail.com>
2023-08-07 13:40:50 +0530

    Java-8-feature Stream-Example Work Flow java-8-optional-method

---
+++ opetional-example/src/OptionalExample.java
@@ -0,0 +1,60 @@
+
+import java.util.Optional;
+/*
+What is optional class in java?
+ A container object  which may or may not contain non value.
+ If value is present or Is-present() return true.
+ The no value is,present the object is considered empty and is-present() return false.
+ Additional method  that depends on presence or absence  of contained value are provided.
+ Additional methods that depend on the presence or absence of a contained value are provided,
+such as orElse() (returns a default value if no value is present) and
+ifPresent() (performs an action if a value is present).
+ */
+
+public class OptionalExample {
+    public static void main(String[] args) {
+        String name = "Komal";
+
+        if (name != null && !name.isEmpty() && name.length() > 4) {
+            System.out.println("Name is not null and not empty and length is greater than 4");
+        } else {
+            System.out.println("Either name is null or name is empty or length is smaller than or equal to 4");
+        }
+
+
+        /*
+        To create an Optional object on given object there are 3 methods
+        1. of -> Use when object is not null otherwise it will throw an NPE
+        2. ofNullable -> Use when object may be null. this method returns Optional object with the value provided
+        if object is null it will return Optional with no value inside it.
+        3. empty -> returns an Optional with no value inside it
+         */
+
+        Optional<String> emptyOptional = Optional.empty();
+        System.out.println("Empty Optional is created with empty object inside it - " + emptyOptional.isPresent());
+        System.out.println("Empty Optional is created with empty object inside it - " + emptyOptional.isEmpty());
+
+        String nullObject = null;
+        Optional<String> optionalString = Optional.ofNullable(nullObject);
+        //With the help of Optional, null checks can be removed from program.
+        if (optionalString.isPresent() && optionalString.get().length() > 4) {
+            System.out.println("Using Optional - Name is not null and not empty and length is greater than 4");
+        } else {
+            System.out.println("Using Optional - Either name is null or name is empty or length is smaller than or equal to 4");
+        }
+
+        Optional<String> optionalString2 = Optional.of("Vikram");
+        //With the help of Optional, null checks can be removed from program.
+        if (optionalString2.isPresent() && optionalString2.get().length() > 4) {
+            System.out.println("Using Optional - Name is not null and not empty and length is greater than 4");
+        } else {
+            System.out.println("Using Optional - Either name is null or name is empty or length is smaller than or equal to 4");
+        }
+    }
+    }
+
+
+
+
+
+
